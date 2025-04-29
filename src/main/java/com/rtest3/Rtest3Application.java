9181326a230ee76cd6549ff0e98aa0bb31f155ec
hyeonrok <wjsgusfhr@naver.com>
2025-04-29 12:54:21 +0900

    Initial commit

---
+++ src/main/java/com/rtest3/Rtest3Application.java
@@ -0,0 +1,13 @@
+package com.rtest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rtest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rtest3Application.class, args);
+	}
+
+}
